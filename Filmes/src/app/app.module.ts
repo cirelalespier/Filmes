203e9d27731efd6f7bc72d7f7493d8cfd88e8669
cirelalespier <lespier.cirela@gmail.com>
2018-04-19 00:01:14 -0300

    versão 1

---
+++ Filmes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeadComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
